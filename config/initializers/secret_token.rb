02c5808f3614cf13065ad250b587e7b221cba5a4
Ancor Cruz <hello@ancorcruz.com>
2013-12-18 10:58:04 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webcran::Application.config.secret_key_base = 'edd4b538293d41a0ef37a322bde7d94acb864056e0eda2a6771e57f56915f971eb18229e5dc7cc611be6cb05da154b4f881ebda0acaa294b0ad6ed13e7e9ce45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Webcran::Application.config.session_store :cookie_store, key: '_webcran_session'
