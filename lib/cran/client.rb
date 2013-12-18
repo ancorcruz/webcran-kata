87cc5518e6af6d7e418fef7e9129ffae7fffc00c
Ancor Cruz <hello@ancorcruz.com>
2013-12-18 15:29:22 +0100

    Fetch package information Refactored specs Added base_uri as an optional parameter in ::fetch method

--- lib/cran/client.rb
+++ lib/cran/client.rb
@@ -1,26 +1,46 @@
 require 'dcf'
 require 'open-uri'
+require 'zlib'
+require 'rubygems/package'
 
 module Cran
   class Client
-    def self.fetch
-      new.fetch
+    def self.fetch base_uri = "http://cran.r-project.org/src/contrib/"
+      new(base_uri).fetch
+    end
+
+    def initialize base_uri
+      @base_uri = base_uri
     end
 
     def fetch
-      fetch_packages_list
-      # Gets extra info of each package from DESCRIPTION file
+      fetch_packages_list.map do |package|
+        package_information = fetch_package_description package["Package"], package["Version"]
+        package.merge package_information
+      end
     end
 
     def fetch_packages_list
       Dcf.parse get_packages_list
     end
 
+    def fetch_package_description name, version
+      tar_file    = Gem::Package::TarReader.new(Zlib::GzipReader.open get_package name, version)
+      description = tar_file.seek("#{name}/DESCRIPTION") { |entry| entry.read }
+      tar_file.close
+
+      Dcf.parse(description).first
+    end
+
 
     private
 
     def get_packages_list
-      open("http://cran.r-project.org/src/contrib/PACKAGES").read
+      open("#{@base_uri}PACKAGES").read
+    end
+
+    def get_package name, version
+      open("#{@base_uri}#{name}_#{version}.tar.gz").read
     end
   end
 end
--- spec/models/cran_client_spec.rb
+++ spec/models/cran_client_spec.rb
@@ -3,18 +3,34 @@ require './lib/cran/client'
 
 module Cran
   describe Client do
-    let(:client) { Client.new }
-    let(:packages_file) { File.open "spec/assets/PACKAGES" }
+    let(:client) { Client.new 'url' }
+    let(:packages_file) { File.open("spec/assets/PACKAGES").read }
+    let(:package_file)  { File.open("spec/assets/ABCExtremes_1.0.tar.gz") }
 
-    describe "fetch" do
-      it "gets all packages information in repository"
+    before do
+      Client.stub(:fetch) { client.fetch }
+      client.stub(:get_packages_list).and_return packages_file
+      client.stub(:get_package).and_return       package_file
+    end
+
+    describe "#fetch" do
+      it "gets all packages information in repository" do
+        package = Client.fetch.first
+        package['Description'].should_not be_empty
+      end
     end
 
     describe "#fetch_packages_list" do
       it "gets the list of packages in repository" do
-        client.should_receive(:open).and_return packages_file
         packages = client.fetch_packages_list
         packages.should have_at_least(1).package
+      end
+    end
+
+    describe "#fetch_package_description" do
+      it "returns the package information hash" do
+        package_description = client.fetch_package_description "ABCExtremes", "1.0"
+        package_description['Description'].should_not be_empty
       end
     end
   end
